f36505d0ef4faa9ffc5bc8b01a5b7264084a3471
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2023-12-27 03:39:18 +0100

    add about-me and skill cards

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnDestroy, ViewEncapsulation} from '@angular/core';
+import {BreakpointService} from "../../services/breakpoint.service";
+import {Subscription} from "rxjs";
+import {BreakpointEnum} from "../../models/breakpoint.enum";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {MatListModule} from "@angular/material/list";
+import {SkillCardComponent} from "./skill-card/skill-card.component";
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [
+    MatGridListModule,
+    MatCardModule,
+    MatListModule,
+    SkillCardComponent
+  ],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnDestroy {
+  currentBreakpoint?: BreakpointEnum;
 
+  readonly BreakpointEnum = BreakpointEnum;
+
+  private subscription?: Subscription;
+  private readonly breakpointService = inject(BreakpointService);
+
+  constructor() {
+    this.subscription = this.breakpointService.activeBreakpoint$.subscribe(breakpoint => {
+      this.currentBreakpoint = breakpoint;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  getColSize(): number {
+    if (this.currentBreakpoint === this.breakpointEnum.XSmall || this.currentBreakpoint === BreakpointEnum.Small) {
+      return 1;
+    } else {
+      return 4;
+    }
+  }
+
+  protected readonly breakpointEnum = BreakpointEnum;
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {CommonModule} from "@angular/common";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatListModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
