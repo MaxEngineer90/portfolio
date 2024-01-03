0db2b7473992e6ccf2b865f992ad8a80a4088772
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2024-01-03 19:46:03 +0100

    add contact form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, ReactiveFormsModule, MatButtonModule, NgIf],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  contactForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [
+      Validators.required,
+      Validators.minLength(15),
+    ]),
+  });
 
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
 }
